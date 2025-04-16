12efb8bdaa77d7485cdd7bca13fc5e84b4c4ff61
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-04-16 16:03:42 +0300

    new addition in in stance

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= '16e454f599fc965323ff03cfa1018b2241401c32232bb79bfa2447b48f8ea181'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cheptoo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 
-
+db = SQLAlchemy(app)
 from app import routes
