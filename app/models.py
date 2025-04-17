ea8806dfdeab8f88705d7385b0b16efdabad6074
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-04-17 15:52:18 +0300

    new files

--- app/models.py
+++ app/models.py
@@ -3,22 +3,24 @@ from app import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+  
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False ,unique=True)
-    firstname = db.Column(db.String(50), nullable=False),
-    lastname = db.Column(db.String(50), nullable=False),
+    firstname = db.Column(db.String(50), nullable=False)  # Uncommented firstname
+    lastname = db.Column(db.String(50), nullable=False)   # Uncommented lastname
     password = db.Column(db.String(100), nullable=False)
     task = db.relationship('Task', backref='user', lazy=True)
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f' {self.firstname} {self.lastname}'
 class Task(db.Model):
-    __tablename__ = 'tasks'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
         return f'<Task {self.title} {self.completed}>'
 
+
+   
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
+from flask import render_template
 from app import app
+from app.models import User
+
+
+
 
 @app.route('/')
 def home():
-    return "<h1>welcome to task manager</h1>"
+    users = User.query.all()
+    return render_template('index.html', users=users)
