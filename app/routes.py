c48f758a2226453d6abd0e7dcd1cb73c49e2bb4e
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-04-22 16:04:15 +0300

    files

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,14 @@ from app.models import User
 @app.route('/')
 def home():
     users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('index.html', users=users, title = 'Home')
+
+@app.route('/task')
+def tasks():
+    users = User.query.all()
+    return render_template('task.html', title = 'Task', users=users)
+
+@app.route('/user')
+def user():
+    users = User.query.all()
+    return render_template('user.html',users=users, title = 'Users')
