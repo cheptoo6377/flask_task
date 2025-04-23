8a578a15ddc90a3bf8f919c86fbc774ab831fa31
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-04-23 16:50:05 +0300

    files

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField ,BooleanField,SubmitField,PasswordField
+from wtforms.validators import DataRequired
 
 
 class Taskform(FlaskForm):
@@ -9,9 +10,9 @@ class Taskform(FlaskForm):
     submit = SubmitField('Submit')
 
 class Userform(FlaskForm):
-    username = StringField('Username')
-    firstname = StringField('First Name')
-    lastname = StringField('Last Name')
-    password = PasswordField('Password')
-    submit = SubmitField('Submit')
+    username = StringField('Username', validators=[DataRequired()])
+    firstname = StringField('First Name',  validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('register')
     
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
-from app import app
+from flask import render_template,redirect,url_for
+from app import app,db
 from app.models import User
 from app.forms import Taskform
 from app.forms import Userform
+
 
 
 
@@ -17,8 +18,13 @@ def tasks():
     form = Taskform()
     return render_template('task.html', title = 'Task',  form=form)
 
-@app.route('/user',methods=['GET', 'POST'])
-def user():
+@app.route('/register',methods=['GET', 'POST'])
+def register():
     form = Userform()
-    users = User.query.all()
-    return render_template('user.html',users=users, title = 'Users',form=form)
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data,firstname=form.firstname.data,lastname=form.lastname.data,password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template('register.html', title = 'Register',form=form)
